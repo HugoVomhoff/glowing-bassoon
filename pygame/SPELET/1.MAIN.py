707fca606028236d4ef0b46eb6f2cc2ed3d13011
Simon Viotti <116351254+SimonV-TE21C@users.noreply.github.com>
2023-01-11 14:59:02 +0100

    Grundläggande kommentarer

--- pygame/SPELET/1.MAIN.py
+++ pygame/SPELET/1.MAIN.py
@@ -1,13 +1,19 @@
+# Importerar och initializar pygame
+
 import pygame
 pygame.init()   
  
+# Hämtar scenhanteraren som heter gamestate från textfilen som också heter gamestate
 from Game_State import game_state
 
+# Sätter fönsternanet till spelets namn och gör så att muspekaren är synlig
 pygame.display.set_caption("Andreas Äventyr")
 pygame.mouse.set_visible(True) 
 
+# Fixar tick för spelet
 Clock = pygame.time.Clock()
 
+# Sätter tickhastigheten och startar igång första scenen
 while True:
     game_state.state_manager()
     pygame.event.clear()
