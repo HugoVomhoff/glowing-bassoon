ed27d518818e6becf28499c55de030e0990db0fd
Hugo Vomhoff <116350779+HugoVomhoff@users.noreply.github.com>
2023-01-19 15:49:13 +0100

    Update Variables.py

--- Dungeon Danger/Variables.py
+++ Dungeon Danger/Variables.py
@@ -25,4 +25,14 @@ Font6_55 = pygame.font.Font("Fonts/Font6.ttf", int(55 *scale))
 Font6_70 = pygame.font.Font("Fonts/Font6.ttf", int(70 *scale))
 Font6_80 = pygame.font.Font("Fonts/Font6.ttf", int(80 *scale))
  
-fonts = [Font1_30, Font1_70, Font1_100, Font1_120, Font6_25, Font6_35, Font6_55, Font6_70, Font6_80] 
+fonts = [Font1_30, Font1_70, Font1_100, Font1_120, Font6_25, Font6_35, Font6_55, Font6_70, Font6_80]
+
+# Sätter alla bilder
+
+Bakgrund1 = pygame.transform.scale(pygame.image.load("Bilder/Scener/Room1 v2 - oilpaint.png"), (screen_Width, screen_Height))
+Character = pygame.image.load("Bilder/Scener/Character - oilpaint.png")
+Character1 = pygame.transform.scale(Character, ((Character.get_width() * scale * 0.9), Character.get_height() * scale * 0.9))
+Bakgrund2 = pygame.transform.scale(pygame.image.load("Bilder/Scener/Shop - oilpaint.png"), (screen_Width, screen_Height))
+Bakgrund3 = pygame.transform.scale(pygame.image.load("Bilder/Scener/Kista - öppen - oilpaint.png"), (screen_Width, screen_Height))
+
+pygame.image.load("Bilder/Scener/Tre dörrar v3- oilpaint.png") 
